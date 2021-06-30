001a22b0679c777c6268f44e7e51a16e378c4548
paruchurivarun02 <paruchurivarun02@gmail.com>
2021-06-30 22:52:28 +0530

    added swap

--- swap.c
+++ swap.c
@@ -18,11 +18,11 @@ void swap_max(int arr[],int l,int n)
 }
 
 void ssort(int arr[],int l){
-  for(int i=0;i<1;i++)
+  for(int i=0;i<l;i++)
   {
     swap_max(arr,l,i);
   }
-for(int i=0;i<1;i++){
+for(int i=0;i<l;i++){
 printf("|%i|\t",arr[i]);
 }
 printf("\n");
