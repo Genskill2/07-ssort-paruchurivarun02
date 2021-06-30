6b5fb514ef129e81fdd5eaa153d9415acb84217a
paruchurivarun02 <paruchurivarun02@gmail.com>
2021-06-30 21:24:47 +0530

    adding swap

---
+++ swap.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+
+
+void swap_max(int arr[],int l,int n)
+{
+  int lar=0;
+  int max=n;
+  for (int i=n;i<l;i++){
+    if(arr[i]>arr[max]){
+      max=i;
+    }
+  }
+   lar=arr[max];
+  arr[max]=arr[n];
+  arr[n]=lar;
+}
+
+void ssort(int arr[],int l){
+  for(int i=0;i<1;i++)
+  {
+    swap_max(arr,l,i);
+  }
+for(int i=0;i<1;i++){
+printf("|%i|\t",arr[i]);
+}
+printf("\n");
+}
